8bb32d2250942bbae80db5bb3b2b074348d374ed
许鑫 <xuxin@cloudcc.com>
2023-01-13 21:36:38 +0800

    chore: 项目添加stylelint支持

--- src/App.vue
+++ src/App.vue
@@ -16,13 +16,15 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <style scoped>
 .logo {
-  height: 6em;
   padding: 1.5em;
+  height: 6em;
   will-change: filter;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
