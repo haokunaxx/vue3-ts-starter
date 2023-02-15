973c13693efbaffa1b97b36f1e0dd8ff7de7361b
许鑫 <xuxin@cloudcc.com>
2023-02-15 22:08:47 +0800

    feat: ElementPlus按需引入

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import vue from '@vitejs/plugin-vue'
 // vue自动导入
 import AutoImport from 'unplugin-auto-import/vite'
 
+// 组件按需导入
+import Components from 'unplugin-vue-components/vite'
+// ElementPlus 按需导入
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 import path from 'path'
 
 const pathSrc = path.resolve(__dirname, 'src')
@@ -16,8 +21,18 @@ export default defineConfig({
       // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
       imports: ['vue'],
 
+      // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
+      resolvers: [ElementPlusResolver()],
+
       include: [/\.[tj]sx?$/, /\.vue$/, /\.vue\?vue/, /\.md$/],
       dts: path.resolve(pathSrc, 'auto-import.d.ts')
+    }),
+    Components({
+      resolvers: [
+        // 自动导入 Element Plus 组件
+        ElementPlusResolver()
+      ],
+      dts: path.resolve(pathSrc, 'components.d.ts')
     })
   ]
 })
